f3427a0336d60b132a0069fe391848b75ab66c1f
Jan <Jan@PCJM>
2015-05-25 19:44:21 +0200

    x

--- src/oca/capture01/staticimport/StaticOutput.java
+++ src/oca/capture01/staticimport/StaticOutput.java
@@ -3,7 +3,7 @@ package oca.capture01.staticimport;
 import static java.lang.System.out;
 
 public class StaticOutput implements StaticPrintable {
-
+	
 	@Override
 	public void out() {
 		out.println("Static import");
